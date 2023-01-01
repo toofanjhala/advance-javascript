71632e49e52ad65927e182ba7444ae6dfe5626b1
toofanjhala <121038143+toofanjhala@users.noreply.github.com>
2023-01-01 11:22:32 +0530

    how to use this inside various things and creating class method static variable question

--- testrun.js
+++ testrun.js
@@ -1,108 +1,84 @@
-// application of call method
+// this in global variable
 
+// var school ="global public school"
+// console.log(this.school)
 
-// let obj1={ age:25}
-// function sum(a,b,c){
+// this in object 
 
-//   return  this.age+a+b+c;
-
+// var school = {
+//     class:11,
+//     totalstudent(){
+//         console.log( `${this.class} have 400 students`)
+//     }
 // }
-// console.log(sum.call(obj1,12,2,1));
+// school.totalstudent();
+
+
+
+ //this inside a function by using call back function
+//  let student={male:40,female:60}
+
+//  function totalstudent(){
+//     console.log(`${this.male+ this.female}`)
+//  }
+
+//  totalstudent.call(student);
+
+
+ //function inside a function 
+
+ let student={male:40,female:60}
+
+ function totalstudent(){
+    //var that=this
+
+    function innerfunction(){
+        console.log(`${this.male+ this.female}`)
+    }
+    innerfunction.call(this);
+
+ }
+
+ totalstudent.call(student);
 
 
 
 
-// application of apply method
+// class student{
+//  static count=0;
+//     constructor(name,age,phn_no,boardmarks)
+//     {
+//         this.name=name;
+//         this.age=age;
+//         this.phn_no=phn_no;
+//         this.boardmarks=boardmarks;
+//         student.count=student.count+1;
+//     }
 
+//    numberofstudent(){
+//      console.log(student.count)
 
-// let obj1={ age:25}
-
-// function sum(a,b,c){
-
-//   return  this.age+a+b+c;
-
-// }
-
-// arr=[1,2,3]
-
-// console.log(sum.apply(obj1,arr));
-
-
-
-
-
-// application of bind method
-
-
-
-// let obj1={ age:25}
-
-// function sum(a,b,c){
-
-//   return  this.age+a+b+c;
-
-// }
-
-// var bound =sum.bind(obj1);
-
-// console.log(bound(2,9,3));
-
-
-
-
-
-// application of object with this and bind method
-
-
-// var student={age:20}
-// function print(){
-
-//     console.log(this.age)
-
-// }
-
-
-
-// var answer=print.bind(student)
-
-// answer();
-
-
-
-
-
-
-
-// currying by using bind method
-
-
-// function multiply(x,y){
-//     console.log(x*y)
-// }
-
-// var multiplybytwo= multiply.bind(this,2)
-// multiplybytwo(5)
-
-// var multiplybyfive= multiply.bind(this,5)
-// multiplybyfive(5)
-
-
-
-
-
-// currying by closure
-
-
-// function multiply(x){
-//     return function(y) {
-//         console.log(x*y);
+//    }
+        
+//     eligibilityForCollege(boardmarks){
+          
+//         let result= this.boardmarks > 40? "eligible for college" : "not eligible for college";
+//         console.log(result);
 //     }
 // }
 
-// let multiplybytwo=multiply(2);
-// multiplybytwo(5)
 
 
 
-// let multiplybynine=multiply(9);
-// multiplybynine(10)
+// let candidate1= new student("rahul",25,25632563,45)
+
+// let candidate2= new student("rekha",18,25632563,30)
+
+// let candidate3= new student("kiran",23,25632563,40)
+
+// let candidate4= new student("himesh",26,25632563,48)
+
+// let candidate5= new student("amit",25,25632563,4)
+
+// candidate1.numberofstudent();
+// candidate4.eligibilityForCollege();
