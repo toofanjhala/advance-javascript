e63f2a9eec3ce6fda9119e58258b7b27d38b673c
toofanjhala <121038143+toofanjhala@users.noreply.github.com>
2023-01-01 12:47:45 +0530

    this arrow function and task 10 design pattern and fat arrow

--- testrun.js
+++ testrun.js
@@ -1,84 +1,71 @@
-// this in global variable
-
-// var school ="global public school"
-// console.log(this.school)
-
-// this in object 
-
-// var school = {
-//     class:11,
-//     totalstudent(){
-//         console.log( `${this.class} have 400 students`)
-//     }
-// }
-// school.totalstudent();
 
 
 
- //this inside a function by using call back function
-//  let student={male:40,female:60}
 
-//  function totalstudent(){
-//     console.log(`${this.male+ this.female}`)
-//  }
-
-//  totalstudent.call(student);
-
-
- //function inside a function 
-
- let student={male:40,female:60}
-
- function totalstudent(){
-    //var that=this
-
-    function innerfunction(){
-        console.log(`${this.male+ this.female}`)
+class student{
+ static count=0;
+    constructor(name,age,phn_no,boardmarks)
+    {
+        this.name=name;
+        this.age=age;
+        this.phn_no=phn_no;
+        this.boardmarks=boardmarks;
+        student.count=student.count+1;
     }
-    innerfunction.call(this);
 
- }
+   numberofstudent(){
+     console.log(student.count)
 
- totalstudent.call(student);
-
-
-
-
-// class student{
-//  static count=0;
-//     constructor(name,age,phn_no,boardmarks)
-//     {
-//         this.name=name;
-//         this.age=age;
-//         this.phn_no=phn_no;
-//         this.boardmarks=boardmarks;
-//         student.count=student.count+1;
-//     }
-
-//    numberofstudent(){
-//      console.log(student.count)
-
-//    }
+   }
         
-//     eligibilityForCollege(boardmarks){
+    eligibilityForCollege(boardmarks){
           
-//         let result= this.boardmarks > 40? "eligible for college" : "not eligible for college";
-//         console.log(result);
-//     }
-// }
+        let result= this.boardmarks > 40? "eligible for college" : "not eligible for college";
+        console.log(result);
+    }
+
+    eligibleforplacement(){
+        if (this.boardmarks>40)
+        {
+            let checkage=()=>{
+                if(this.age>20){
+            
+                console.log(`${this.name} congratulation for eligibility`);}
+                else
+                console.log("not eligible because of age below 20");
+            }
+            return checkage();
+        }
+        else
+        console.log("not eligible beacuse of boardmarks less than 40");
+    }
+}
 
 
 
 
-// let candidate1= new student("rahul",25,25632563,45)
+let candidate1= new student("rahul",25,25632563,45)
 
-// let candidate2= new student("rekha",18,25632563,30)
+let candidate2= new student("rekha",18,25632563,30)
 
-// let candidate3= new student("kiran",23,25632563,40)
+let candidate3= new student("kiran",19,25632563,43)
 
-// let candidate4= new student("himesh",26,25632563,48)
+let candidate4= new student("himesh",26,25632563,48)
 
-// let candidate5= new student("amit",25,25632563,4)
+let candidate5= new student("amit",14,25632563,34)
 
-// candidate1.numberofstudent();
-// candidate4.eligibilityForCollege();
+//candidate1.numberofstudent();
+//candidate4.eligibilityForCollege();
+candidate1.eligibleforplacement();
+candidate2.eligibleforplacement();
+candidate3.eligibleforplacement();
+candidate4.eligibleforplacement();
+candidate5.eligibleforplacement();
+
+
+
+
+
+
+
+
